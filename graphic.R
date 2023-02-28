5a1a73f3579812f4dc9a0582748566f5a39e2a27
Ana <87942437+AnaStadelhofer@users.noreply.github.com>
2023-02-27 21:44:33 -0300

    Add files via upload

--- graphic.R
+++ graphic.R
@@ -18,7 +18,7 @@ berkeley2 %>% mutate(Departamento = str_c("Dep ", Departamento)) %>%
   pivot_longer(cols = Admitidos:Candidado, names_to = 'Aceitos', values_to = 'n') %>% 
   ggplot(aes(x = Genero, y = n, fill = Aceitos)) +
   geom_col(position = 'dodge') +
-  scale_fill_viridis_d(NULL, option = 'A', end = 0.6) +
+  #scale_fill_viridis_d(NULL, option = 'A', end = 0.6) +
   xlab(NULL) + 
   theme(panel.grid = element_blank()) +
   facet_wrap(~Departamento)
